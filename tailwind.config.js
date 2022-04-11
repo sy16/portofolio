9896e24a9a63ba6b185d273b0cf9338ebb4c5d0f
sy16 <uba.pbt@gmail.com>
2022-04-11 08:20:48 +0800

    commit pertama

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#1c1917',
+      },
+    },
+  },
+  plugins: [],
+}
